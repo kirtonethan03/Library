5ef442fbd5f46555c1f3481112497b6936a18f81
ETHAN\cubeg <kirton.ethan03@gmail.com>
2024-11-07 19:07:58 +0000

    Added more files from my personal machine

---
+++ FishyForms - Latest/FishUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FishUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FishUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8537f8b6-f6b0-4333-bc4e-d3090e1dfa70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FishyForms - Latest/FishUnitTests/ServerUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FishUnitTests
+{
+    [TestClass]
+    public class ServerUnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void NoImageFound()
+        {
+            //When the server is called with a name of a fish that is not contained in the resources an exception is thrown.
+
+            var imageServer = new MediaServer.ResourceFileServer();
+            var image = imageServer.LoadImage("This Fish Doesnt Exist");
+        }
+
+        [TestMethod]
+        public void ImageFound()
+        {
+            //When the server is called with a name of a fish that is contained in the resources it is returned as an image.
+
+            var imageServer = new MediaServer.ResourceFileServer();
+            var image = imageServer.LoadImage("OrangeFish");
+
+            Assert.AreEqual(image.GetType(), typeof(System.Drawing.Bitmap));
+        }
+    }
+}
