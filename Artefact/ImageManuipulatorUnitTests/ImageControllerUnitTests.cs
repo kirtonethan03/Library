21a3812e56e548aa3f54da3d784bf6aaecb2352b
ETHAN\cubeg <kirton.ethan03@gmail.com>
2024-11-07 18:40:25 +0000

    Initial commit

---
+++ Artefact/ImageManuipulatorUnitTests/ImageControllerUnitTests.cs
@@ -0,0 +1,27 @@
+﻿/// Authors: Tom & Ethan
+using ImageManipulatorController.Validator;
+using ImageManuipulatorUnitTests.TestData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageManuipulatorUnitTests
+{
+    /// <summary>
+    /// This test helps verify the functionality of image loading 
+    /// and validation within the image controller
+    /// </summary>
+    [TestClass]
+    public class ImageControllerUnitTests
+    {
+        [TestMethod]
+        public void LoadImages_ValidateImages()
+        {
+            var imageStore = new DummyImageStore();
+            var images = imageStore.LoadImages();
+
+            var imageValidator = new PngValidator();
+            imageValidator.ValidateImages(images);
+            // checks that there are 4 images objects 
+            Assert.AreEqual(images.Count, 4);
+        }
+    }
+}
---
+++ Artefact/ImageManuipulatorUnitTests/TestData/DummyImageStore.cs
@@ -0,0 +1,47 @@
+﻿/// Authors: Tom & Ethan
+using ImageManipulatorModel;
+using System.Collections.Generic;
+
+namespace ImageManuipulatorUnitTests.TestData
+{
+    /// <summary>
+    /// Provides a simple implementation of the IImageStore interface, returns a 
+    /// list of dummy Image objects for testing purposes, simulating the behavior of an image store
+    /// </summary>
+    public class DummyImageStore : IImageStore
+    {
+        public List<Image> LoadImages()
+        {
+            // use of a list for the dummy images
+            return new List<Image>()
+            {
+                new Image()
+                {
+                    ImageID = 1,
+                    FileName = "ImagePath1.png"
+                },
+                new Image()
+                {
+                    ImageID = 2,
+                    FileName = "ImagePath2.png"
+                },
+                new Image()
+                {
+                    ImageID = 3,
+                    FileName = "ImagePath3.png"
+                },
+                new Image()
+                {
+                    ImageID = 4,
+                    FileName = "ImagePath4.png"
+                },
+                // here is our file implemented to be caught by the test to show that the validator works
+                new Image()
+                {
+                    ImageID = 99,
+                    FileName = "NotARealpng.jpg"
+                }
+            };
+        }
+    }
+}
